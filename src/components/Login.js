a63c533c2966836f203c659fcd12adadd5c34118
(hyetoria) <(businesshyeji@gmail.com)>
2022-12-26 09:03:38 +0900

    created login js /LoginPage.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,40 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { useForm } from 'react-hook-form';
-import { signIn } from '../utils/useAPI';
-import { useNavigate } from 'react-router-dom';
+import React, {useState} from "react";
+import styled from "styled-components";
+import {useForm} from "react-hook-form";
+import {signIn} from "../utils/useAPI";
+import {useNavigate} from "react-router-dom";
+
 const Container = styled.div``;
 
 const LoginForm = styled.form`
   input {
   }
+
   button {
     border: 2px solid white;
     border-radius: 5px;
     background-color: blue;
-    color: white;
+    color: RED;
   }
 `;
-const Login = (props) => {
-  // const history = useNavigate();
+const Login = () => {
+  const history = useNavigate();
 
   const [loginInfo, setLoginInfo] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
   // react hook form 사용해보기(렌더링 비용 최소화)
-  const { register, handleSubmit } = useForm();
+  const {register, handleSubmit} = useForm();
   const onSubmit = async (data) => {
-    const { accessToken, user } = await signIn(data);
+    const {accessToken, user} = await signIn(data);
     setLoginInfo(data);
     if (!user) {
-      alert('이메일 혹은 비밀번호가 일치하지 않습니다.');
+      alert("이메일 혹은 비밀번호가 일치하지 않습니다.");
     } else {
-      sessionStorage.setItem('accessToken', accessToken);
+      sessionStorage.setItem("accessToken", accessToken);
       alert(`hello, ${user.displayName}!!`);
-      window.location.reload();
+      history("/user");
     }
   };
 
@@ -44,13 +46,13 @@ const Login = (props) => {
         <input
           type="email"
           placeholder="Email ID"
-          {...register('email', { required: true })}
+          {...register("email", {required: true})}
         />
         password
         <input
           type="password"
           placeholder="Password"
-          {...register('password', { required: true })}
+          {...register("password", {required: true})}
         />
         <button>로그인!</button>
       </LoginForm>
