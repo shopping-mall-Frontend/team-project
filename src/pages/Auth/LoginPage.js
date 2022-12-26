435bbe3dfe4ec24054f6e8c8d8fd0c852c50c345
seungyoonLee <syoon624@naver.com>
2022-12-26 17:13:48 +0900

    feat: 로그인 화면 구성 변경

--- src/pages/Auth/LoginPage.js
+++ src/pages/Auth/LoginPage.js
@@ -4,25 +4,10 @@ import Header from '../../components/Header';
 
 const LoginPage = () => {
   return (
-    <div>
-      {user ? (
-        <div>
-          <p> 현재 사용자: {user.displayName} </p>
-          <button
-            onClick={async () => {
-              logoutButtonHandle();
-              await auth('logout');
-              window.location.reload();
-            }}
-          >
-            로그아웃하기
-          </button>
-          <Link to="/">메인 페이지로 이동</Link>
-        </div>
-      ) : (
-        <Login />
-      )}
-    </div>
+    <>
+      <Header />
+      <Login />
+    </>
   );
 };
 
