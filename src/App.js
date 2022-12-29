d47ce25db3f21832c67adcbad805fab0dc41f257
seungyoonLee <syoon624@naver.com>
2022-12-30 00:28:29 +0900

    feat: Outlet 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import {
   MainPage,
@@ -11,6 +11,16 @@ import {
   UserPage,
 } from './pages/index';
 import { getAllProduct } from './utils/useAPI';
+import SearchPage from './pages/SearchPage';
+
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+      {/* <Footer /> */}
+    </>
+  );
+};
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -27,15 +37,18 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<MainPage products={products} setProducts={setProducts} />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/category/:category/:brand" element={<CategoryPage products={categoryItems} />} />
-      <Route path="/category/:category" element={<CategoryPage products={categoryItems} />} />
-      <Route path="/product/:id" element={<ProductdetailsPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/order" element={<OrderPage/>} />
-      <Route path="/user" element={<UserPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage products={products} setProducts={setProducts} />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="category/:category/:brand" element={<CategoryPage products={categoryItems} />} />
+        <Route path="category/:category" element={<CategoryPage products={categoryItems} />} />
+        <Route path="product/:id" element={<ProductdetailsPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="order" element={<OrderPage />} />
+        <Route path="user" element={<UserPage />} />
+      </Route>
     </Routes>
   );
 };
