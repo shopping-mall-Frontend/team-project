4abb1797d6898c17333ac0ffdcd63ec59384fd36
seungyoonLee <syoon624@naver.com>
2022-12-30 02:39:41 +0900

    feat: Search bar 검색 자동 완성 기능 추가

---
+++ src/components/search/ResultsBox.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const ResultsBox = ({ products, openSearchBox }) => {
+  const navigate = useNavigate();
+
+  if (openSearchBox) {
+    return (
+      <Container>
+        {products.slice(0, 10).map((item) => {
+          return (
+            <li
+              key={item.id}
+              onClick={() => {
+                navigate(`/product/${item.id}`);
+              }}
+            >
+              <img alt="이미지" src={item.thumbnail} />
+              <p>{item.title}</p>
+              <span>${item.price}</span>
+            </li>
+          );
+        })}
+      </Container>
+    );
+  }
+};
+
+const Container = styled.ul`
+  border: 1px solid black;
+  background-color: white;
+  margin-left: 30px;
+  li {
+    position: relative;
+    width: 400px;
+    cursor: pointer;
+    display: flex;
+    height: 50px;
+    align-items: center;
+    padding: 10px;
+    img {
+      display: block;
+      height: 100%;
+      padding-right: 20px;
+    }
+    &:hover {
+      background-color: #dee2e6;
+    }
+    p {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      padding-right: 100px;
+    }
+    span {
+      position: absolute;
+      right: 10px;
+    }
+  }
+`;
+
+export default ResultsBox;
