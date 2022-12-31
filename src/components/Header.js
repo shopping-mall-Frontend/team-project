c129705ef0f4bc1c2ca296aa3d7993d1f443b026
(hyetoria) <(businesshyeji@gmail.com)>
2022-12-31 09:22:20 +0900

    Revert "Design: user 이름 길이에 따라 달라지는 배열수정"
    This reverts commit f1be8dfa881fd457681a4bbfcbc7b520b5351314.

--- src/components/Header.js
+++ src/components/Header.js
@@ -67,6 +67,22 @@ const Header = React.memo(() => {
         <div className="logo">
           <Link to={'/'}>N4</Link>
         </div>
+        <nav>
+          <ul className="nav__links">
+            <li>
+              {isLogin ? (
+                <Link to={'/'} onClick={() => logout()}>
+                  LogOut
+                </Link>
+              ) : (
+                <Link to={'/login'}>Login/out</Link>
+              )}
+            </li>
+            <li>
+              <Link to={'/Cart'}>Chart</Link>{' '}
+            </li>
+          </ul>
+        </nav>
       </header>
       <StyledCategory>
         <ul>
