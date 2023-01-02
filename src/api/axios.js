02ffd743ff98c4defc1013d0db19c338a9622fb0
seungyoonLee <syoon624@naver.com>
2023-01-02 13:57:25 +0900

    chore: 기본 Url 세팅

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const { REACT_APP_API_KEY, REACT_APP_USERNAME } = process.env;
-const requestUrl = 'api';
+const requestUrl = 'https://asia-northeast3-heropy-api.cloudfunctions.net/api';
 
 const instance = axios.create({
   baseURL: requestUrl,
